216cba12954569d07cbe0cba926c9c6950cdfe1a
lukaio3D <lukas.schaab@lukaio.de>
2023-12-15 05:40:24 +0100

    toogle button for credits

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ geschBtn.addEventListener("click", function(){
     document.querySelector("video").play();
     setTimeout( elephant.setAttribute("visible", "true") , 5000);
 })
+
+const creditBtn = document.getElementById('creditBtn');
+const credits = document.getElementById("credits");
+let creditFlag = true;
+
+credits.style.visibility = "hidden";
+
+creditBtn.addEventListener("click", function(){
+    if (creditFlag){
+        credits.style.visibility = "visible";
+        creditFlag = false;
+    }
+    else {
+        credits.style.visibility ="hidden";
+        creditFlag = true;
+    }
+})
